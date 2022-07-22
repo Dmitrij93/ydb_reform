eb768c13701bc45c70a0e2a0f8178604a3a70fd7
unknown <genius-alfa@mail.ru>
2022-07-22 14:49:04 +0300

    append funcs scanValues, setValues, type _Repo

--- template.go
+++ template.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"text/template"
 )
 
 func createTargetFile(fileName string) {
@@ -14,11 +15,14 @@ func createTargetFile(fileName string) {
 	if err != nil {
 		log.Fatalf("Error when opening %s file: %s", targetFileName, err)
 	}
-	imp := strings.Join(parsedTables_.imp, "\n")
-	_, err = io.WriteString(file, imp)
+	text := strings.Join(parsedTables_.Imp, "\n")
+	_, err = io.WriteString(file, text)
 	if err != nil {
 		log.Fatalf("Error when writing imports in target file: %s", err)
 	}
+	appendFuncs(*file)
+	file.Close()
+
 }
 
 func createFile(fileName string) {
@@ -27,4 +31,55 @@ func createFile(fileName string) {
 		log.Printf("Error when creating file: %s", err)
 	}
 	defer file.Close()
+}
+
+func appendFuncs(file os.File) {
+
+	text := []string{
+		`
+func (u *{{ .St.NameTable }}) scanValues() []named.Value {
+	return []named.Value{
+	{{- range $i, $x := .St.Table_ }}
+		named.
+		{{- if eq $i 0 }}Reqired
+		{{- else }}{{if index $x.Type 0}}OptionalWithDefault
+		{{- else }}Optional
+		{{- end }}{{ end -}}
+		("{{ index $x.YDBField 0 }}", &u.{{ $x.Field }}),
+		{{- end }}
+	}
+}
+`,
+		`
+func (u *{{ .St.NameTable }}) setValues() []table.ParameterOption {
+	return []table.ParameterOption{
+		{{- range $i, $x := .St.Table_ }}
+		table.ValueParam("${{ $x.Field }}", types.
+		{{- if index $x.Type 0 }}
+		{{- else }}Nullable
+		{{- end }}{{ $x.YDBType }}Value(u.{{ $x.Field }})),
+		{{- end }}
+
+		table.ValueParam("$UserID", types.Uint64Value(u.UserID)),
+		table.ValueParam("$Username", types.UTF8Value(u.Username)),
+		table.ValueParam("$FirstName", types.UTF8Value(u.FirstName)),
+		table.ValueParam("$LastName", types.UTF8Value(u.LastName)),
+		table.ValueParam("$LanguageCode", types.UTF8Value(u.LanguageCode)),
+	}
+}
+`,
+		`
+type {{ .St.NameTable }}Repo struct {
+	DB ydb.Connection
+}
+`,
+	}
+	for i, f := range text {
+		t := template.New("")
+		t.Parse(f)
+		if err := t.Execute(&file, parsedTables_); err != nil {
+			log.Fatalf("Error when writing struct in target file (record %d): %s", i, err)
+		}
+	}
+
 }
