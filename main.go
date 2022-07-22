924bdab68b86908bf82d53a0c4b4887be4f11d6f
unknown <genius-alfa@mail.ru>
2022-07-22 14:45:17 +0300

    update parse table, create YDBType

--- main.go
+++ main.go
@@ -8,17 +8,44 @@ import (
 	"strings"
 )
 
-type fieldStruct struct{ field, type_ string }
-type table map[fieldStruct][]string
+type table struct {
+	Field    string
+	Type     []string
+	YDBField []string
+	YDBType  string
+}
+type namedTable struct {
+	NameTable string
+	Table_    []table
+}
 type parsedTables struct {
-	imp []string
-	st  map[string]table
+	Imp []string
+	St  namedTable
 }
 
 var parsedTables_ parsedTables
 var imp []string
 var currentlyNameTable string = ""
 var flag string
+var ydbType = map[string]string{
+	"string":     "UTF8",
+	"bool":       "Bool",
+	"uint":       "Uint",
+	"uint8":      "Uint8",
+	"uint16":     "Uint16",
+	"uint32":     "Uint32",
+	"uint64":     "Uint64",
+	"int":        "Int",
+	"int8":       "Int8",
+	"int16":      "Int16",
+	"int32":      "Int32",
+	"int64":      "Int64",
+	"float32":    "Float",
+	"float64":    "Float",
+	"complex64":  "",
+	"complex128": "",
+	"time.Time":  "DatetimeValueFromTime",
+}
 
 func parse(text string) {
 	if (len(text) < 4 || text[:4] != "type") && flag == "" {
@@ -26,16 +53,16 @@ func parse(text string) {
 	} else if len(text) > 4 && text[:4] == "type" {
 		flag = "parsing struct"
 		currentlyNameTable = strings.Fields(text)[1]
-		parsedTables_.imp = imp
-		parsedTables_.st[currentlyNameTable] = table{}
+		parsedTables_.Imp = imp
+		parsedTables_.St = namedTable{currentlyNameTable, []table{}}
 	} else if text == "}" {
 		flag = "parse is complete"
 	} else if flag == "parsing struct" {
 		tableField := strings.Fields(text)
-		fieldstruct := fieldStruct{tableField[0], tableField[1]}
-		ydbFieldString := strings.Split(tableField[2], `"`)[1]
-		ydbField := strings.Split(ydbFieldString, ",")
-		parsedTables_.st[currentlyNameTable][fieldstruct] = ydbField
+		typeField := strings.Split(tableField[1], `*`)
+		ydbField := strings.Split(strings.Split(tableField[2], `"`)[1], ",")
+		fieldstruct := table{tableField[0], typeField, ydbField, ydbType[typeField[len(typeField)-1]]}
+		parsedTables_.St.Table_ = append(parsedTables_.St.Table_, fieldstruct)
 	}
 }
 
@@ -61,7 +88,7 @@ func main() {
 			count := 0
 			flag = ""
 			imp = []string{}
-			parsedTables_ = parsedTables{[]string{}, map[string]table{}}
+			parsedTables_ = parsedTables{[]string{}, namedTable{}}
 			for fileScanner.Scan() {
 				text := fileScanner.Text()
 				if count == 0 && text != "//ydb_reform" {
